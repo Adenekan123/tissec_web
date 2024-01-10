9c39785f835b0cffc1dd11ac89e269840e335833
rhemmy2022 <olorunfemiabiodun05@gmail.com>
2024-01-10 04:45:56 -0800

    falanx-header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,20 @@
-
+import logo from "../../assets/images/FalanxCyber_Yellow_RGB_HighRes_Wavenet-2.png"
 export const Header = () => {
   return (
-    <div className="border px-8 py-4">Header</div>
+    <div className="border px-8 py-4">
+      <header className="flex space-x-32 justify-between w-full ">
+        <div className="">
+          <img className="w-32" src={logo} alt="" />
+        </div>
+        <div className="flex space-x-4 text-lg font-bold mt-4 mr-16 justify-end font-normal">
+
+          <button className="text-blue-950 hover:text-amber-400 py-2 px-4">Our services</button> 
+          <button className="text-blue-950 hover:text-amber-400 py-2 px-0">Who we are</button>
+          <a href="" className="border-2 border-amber-400 text-amber-400 hover:bg-amber-400 hover:text-white py-2 px-4">Contact us</a>
+
+        </div>
+      </header>
+    </div>
   )
 }
 
