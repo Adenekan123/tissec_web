cae4fddc4c69b2d24711ca8a2a75b1843e07f868
Adenekan123 <mr.nekan123@gmail.com>
2024-01-14 15:06:02 +0100

    added who-we-are

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,12 @@ import { Carousel } from "../../components";
 import { HomeBaner } from "../../components";
 import { homeBannerContents } from "../../static/home.static";
 import WhatWeDo from "./components/what-we-do";
+import WhoWeAre from "./components/who-we-are";
 
 const Home = () => {
   return (
     <>
       <section className="banner bg-cover bg-top-right min-h-max relative overflow-hidden">
-      
         <Carousel>
           {homeBannerContents.map((content, i) => (
             <div key={"slide" + i} className="embla__slide ">
@@ -16,6 +16,9 @@ const Home = () => {
           ))}
         </Carousel>
         <WhatWeDo />
+      </section>
+      <section>
+        <WhoWeAre />
       </section>
     </>
   );
