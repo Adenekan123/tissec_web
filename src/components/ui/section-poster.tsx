2e451ebf24acfc0fc342d6c35e281be65fd2bad1
rhemmy2022 <olorunfemiabiodun05@gmail.com>
2024-02-07 08:33:29 -0800

    penetration-testing update

---
+++ src/components/ui/section-poster.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+export const SectionPoster = ({ image, children }: { image: string, children: ReactNode }) => {
+    return (
+        <section className="p-4 md:p-0">
+            <div className={`md:bg-[url('${image}')] bg-cover pb-24 w-full relative`}>
+                <div className="md:flex flex-row justify-end">
+                    {children}
+                </div>
+            </div>
+        </section>
+    )
+}
