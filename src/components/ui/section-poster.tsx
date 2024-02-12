34d758ce2811fbc9a7a8b0ba548abc6824bba7e4
Adenekan123 <mr.nekan123@gmail.com>
2024-02-12 21:53:28 +0100

    managed-endpoint update

--- src/components/ui/section-poster.tsx
+++ src/components/ui/section-poster.tsx
@@ -3,8 +3,8 @@ import { ReactNode } from "react"
 export const SectionPoster = ({ image, children }: { image: string, children: ReactNode }) => {
     return (
         <section className="p-4 md:p-0">
-            <div className={`md:bg-[url('${image}')] bg-cover pb-24 w-full relative`}>
-                <div className="md:flex flex-row justify-end border">
+            <div className={`md:bg-[url('${image}')] bg-cover bg-left pb-24 w-full relative`}>
+                <div className="md:flex flex-row justify-end">
                     {children}
                 </div>
             </div>
